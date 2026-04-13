315720e0e41d04e5c55db9687bc6d27b6b888ff5
Boqiang Liang <lbq999@gmail.com>
2026-04-12 22:53:30 -0400

    test push

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,7 +12,7 @@ SRC_DIR = os.path.join(ROOT_DIR, "src")
 if SRC_DIR not in sys.path:
     sys.path.insert(0, SRC_DIR)
 
-import config
+import config 
 
 
 class PostBridgeConfigTests(unittest.TestCase):
